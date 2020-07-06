812cd7c00f14b124b9287af1b7297d116fe5c194
Dan Lawson <dlawson@entechserv.com>
2020-07-06 12:24:33 -0500

    Removed a stray pdb.set_trace() in ingestor

--- armory2/armory_main/included/modules/Ingestor.py
+++ armory2/armory_main/included/modules/Ingestor.py
@@ -110,7 +110,7 @@ class Module(ModuleTemplate):
                 for line in ips:
 
                     if line.strip():
-                        pdb.set_trace()
+                        # pdb.set_trace()
                         if "/" in line or "-" in line:
                             self.process_cidr(line, args.label)
 
