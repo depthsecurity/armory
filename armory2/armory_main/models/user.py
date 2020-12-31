4161c37076f9038881b73d788c2ecb88e02038fe
Dan Lawson <dlawson@digital-panther.com>
2020-12-31 10:38:14 -0600

    Modified job title to be longer because wtf why does someone have a massive job title

--- armory2/armory_main/models/user.py
+++ armory2/armory_main/models/user.py
@@ -10,7 +10,7 @@ class User(BaseModel):
     last_name = models.CharField(max_length=128)
     user_name = models.CharField(max_length=128)
     domain = models.ForeignKey(BaseDomain, on_delete=models.CASCADE)
-    job_title = models.CharField(max_length=128)
+    job_title = models.CharField(max_length=256)
     location = models.CharField(max_length=128)
 
 
