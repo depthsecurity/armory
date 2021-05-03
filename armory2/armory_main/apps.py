b5406b131c9666b7b51665e5bf4b58c9ec612705
Mark Hedrick <MarkoH17@users.noreply.github.com>
2021-05-03 08:33:57 -0600

    Added Navigation bar
    CHANGED - Renamed main_template.html to base.html
    CHANGED - Page titles are now passed through the view
    CHANGED - Minor styling update
    ADDED - Navigation bar for quick navigation between webapps
    ADDED - Context Processor, Filters, and Simple tags to support nav bar function

--- armory2/armory_main/apps.py
+++ armory2/armory_main/apps.py
@@ -1,5 +1,44 @@
 from django.apps import AppConfig
+import os
+import json
+import glob
+from django.conf import settings
 
+def get_armory_webapps():
+        """
+        Return dictionary of webapps available in Armory
+        """
+        webapps = {}
+        print("Called Get Armory Webapps")
+        app_paths = glob.glob(f"{'/'.join(os.path.realpath(__file__).split('/')[:-1])}/included/webapps/*/config.json")
+
+        if settings.ARMORY_CONFIG['ARMORY_CUSTOM_WEBAPPS']:
+            for path in settings.ARMORY_CONFIG['ARMORY_CUSTOM_WEBAPPS']:
+                for webapp in glob.glob(f"{'/'.join(os.path.realpath(path).split('/'))}/*/config.json"):
+                    app_paths.append(webapp)
+
+        for app_config in app_paths:
+            with open(app_config, 'r') as f:
+                    app_config = json.load(f)
+                    apps_key = app_config['name'] if app_config['name'] else module_config_path.split("/")[-2]
+                    webapps[apps_key] = app_config
+        
+        return webapps
+
+def get_armory_webapps_grouped(webapps):
+    webapps_grouped = {}
+    for app, app_data in webapps.items():
+        app_category = app_data['category']
+        if app_category in webapps_grouped:
+            webapps_grouped[app_category].append(app_data)
+        else:
+            webapps_grouped[app_category] = [app_data]
+    return webapps_grouped
 
 class ArmoryMainConfig(AppConfig):
     name = 'armory2.armory_main'
+    webapps = get_armory_webapps()
+    webapps_grouped = get_armory_webapps_grouped(webapps)
+    
+
+    
