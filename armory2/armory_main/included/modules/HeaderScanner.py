d99cc73a0152bf659adbf2a3512cc5e108d9ee0a
Dan Lawson <dlawson@digital-panther.com>
2022-11-16 15:34:42 -0600

    Updated headerscanner to work with virtualhosts

--- armory2/armory_main/included/modules/HeaderScanner.py
+++ armory2/armory_main/included/modules/HeaderScanner.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+from armory2.armory_main.included.utilities import get_urls
 from armory2.armory_main.models import Port, Domain, IPAddress
 from armory2.armory_main.included.ModuleTemplate import ModuleTemplate
 import requests
@@ -26,7 +27,6 @@ def check_if_ip(txt):
         return False
 
 
-
 class Module(ModuleTemplate):
 
     name = "HeaderScanner"
@@ -51,56 +51,88 @@ class Module(ModuleTemplate):
         self.options.add_argument(
             "--rescan", help="Rescan URLs already processed", action="store_true"
         )
+        self.options.add_argument(
+            "--virtualhost", "-v", help="Scan virtualhosts", action="store_true"
+        )
 
     def run(self, args):
         urls = []
         if args.url:
             urls.append(args.url)
-            
+
         if args.file:
+
             url = open(args.file).read().split("\n")
             for u in url:
                 if u:
                     urls.append(u)
 
         if args.import_db:
-
-                    
-            if args.rescan:
-                urls += run(scope_type="active")
+            if args.virtualhost:
+                if args.rescan:
+                    urls += get_urls.get_urls_with_virtualhosts(scope_type="active")
+                else:
+                    urls += get_urls.get_urls_with_virtualhosts(
+                        scope_type="active", tool=self.name, args=""
+                    )
             else:
-                urls += run(scope_type="active", tool=self.name, args="")
-
-
+                if args.rescan:
+                    urls += get_urls.run(scope_type="active")
+                else:
+                    urls += get_urls.run(scope_type="active", tool=self.name, args="")
 
         if urls:
+
             pool = ThreadPool(int(args.threads))
             data = [(u, args.timeout) for u in urls]
             # pdb.set_trace()
             results = pool.map(process_urls, data)
             display_new("Adding headers to the database")
-            
-            for headers, cookies in results:
-                if len(list(headers.keys())) > 0:
-                    h = list(headers.keys())[0]
+
+            for res in results:
+                virtualhost = res["virtualhost"]
+                headers = res["headers"]
+                cookies = res["cookies"]
+                if len(headers) > 0:
+                    h = res["url"]
                     dom, dom_type, scheme, port = get_url_data(h)
                     display("Processing headers and cookies from URL {}".format(h))
-                
-                    if dom_type == 'ip':
+
+                    if dom_type == "ip":
                         ip, created = IPAddress.objects.get_or_create(ip_address=dom)
-                        
-                        ip.add_tool_run(tool=self.name)
+
+                        ip.add_tool_run(
+                            tool=self.name,
+                            virtualhost=res["virtualhost"],
+                            port=int(port),
+                        )
                         # pdb.set_trace()
-                        p, created = Port.objects.get_or_create(ip_address=ip, port_number=port, service_name=scheme, proto="tcp")
-                        if not p.meta.get('headers'):
-                            p.meta['headers'] = {} 
-                        p.meta['headers'][dom] = headers[h]
-                        
-                        if not p.meta.get('cookies'):
-                            p.meta['cookies'] = {} 
-                        
-                        p.meta['cookies'][dom] = cookies.get(h, [])
-                        
+                        p, created = Port.objects.get_or_create(
+                            ip_address=ip,
+                            port_number=port,
+                            service_name=scheme,
+                            proto="tcp",
+                        )
+                        if not p.meta.get("headers"):
+                            p.meta["headers"] = {}
+
+                        if (
+                            p.meta["headers"].get(virtualhost, False) == False
+                            or type(p.meta["headers"][virtualhost]) != dict
+                        ):
+                            p.meta["headers"][virtualhost] = {}
+
+                        try:
+                            p.meta["headers"][virtualhost][dom] = headers
+                        except Exception as e:
+                            print(e)
+
+                        if not p.meta.get("cookies"):
+                            p.meta["cookies"] = {}
+                        if p.meta["cookies"].get(virtualhost, False) == False:
+                            p.meta["cookies"][virtualhost] = {}
+                        p.meta["cookies"][virtualhost][dom] = cookies
+
                         p.save()
 
                     else:
@@ -110,38 +142,46 @@ class Module(ModuleTemplate):
 
                         for ip in domain.ip_addresses.all():
 
-                            p, created = Port.objects.get_or_create(ip_address=ip, port_number=port, service_name=scheme, proto="tcp")
-                            if not p.meta.get('headers'):
-                                p.meta['headers'] = {} 
-                            p.meta['headers'][dom] = headers[h]
-                            
-                            if not p.meta.get('cookies'):
-                                p.meta['cookies'] = {} 
-                            p.meta['cookies'][dom] = cookies.get(h, [])
-                            
-                            p.save()
+                            p, created = Port.objects.get_or_create(
+                                ip_address=ip,
+                                port_number=port,
+                                service_name=scheme,
+                                proto="tcp",
+                            )
+                            if not p.meta.get("headers"):
+                                p.meta["headers"] = {}
 
-            
+                            if p.meta["headers"].get(virtualhost, False) == False:
+                                p.meta["headers"][virtualhost] = {}
+                            p.meta["headers"][virtualhost][dom] = headers
+
+                            if not p.meta.get("cookies"):
+                                p.meta["cookies"] = {}
+                            if p.meta["cookies"].get(virtualhost, False) == False:
+                                p.meta["cookies"][virtualhost] = {}
+                            p.meta["cookies"][virtualhost][dom] = cookies
+
+                            p.save()
 
 
 def get_url_data(url):
 
-    scheme = url.split(':')[0]
-    if url.count(':') == 2:
-        port = url.split(':')[-1]
-    elif scheme == 'http':
+    scheme = url.split(":")[0]
+    if url.count(":") == 2:
+        port = url.split(":")[-1]
+    elif scheme == "http":
         port = 80
-    elif scheme == 'https':
+    elif scheme == "https":
         port = 443
     else:
         port = 0
-    
-    dom = url.split('/')[2].split(':')[0]
+
+    dom = url.split("/")[2].split(":")[0]
     try:
-        [int(i) for i in dom.split('.')]
+        [int(i) for i in dom.split(".")]
         # If we made it here, it is an IP
 
-        dom_type = 'ip'
+        dom_type = "ip"
     except:
         dom_type = "domain"
 
--- armory2/armory_main/included/utilities/header_tools.py
+++ armory2/armory_main/included/utilities/header_tools.py
@@ -9,10 +9,15 @@ from armory2.armory_main.included.utilities.color_display import (
     display_new,
 )
 
+
 def process_urls(data):
     # silence insecure url warnings
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-    u = data[0]
+    if type(data[0]) == list:
+        u, virtualhost = data[0]
+    else:
+        u = data[0]
+        virtualhost = None
     timeout = data[1]
     blacklist = [
         "Date",
@@ -23,19 +28,24 @@ def process_urls(data):
         "Content-Encoding",
         "Vary",
     ]
-    new_headers = {}
-    new_cookies = {}
+    data = {"virtualhost": virtualhost, "url": u, "headers": [], "cookies": {}}
 
-    display("Processing %s" % u)
+    if virtualhost:
+        display(f"Processing {u} ({virtualhost})")
+    else:
+        display("Processing %s" % u)
     try:
-        res = requests.get(u, timeout=int(timeout), verify=False, headers={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 14.5; rv:10.0) Gecko/20100101 Firefox/89.0"})
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 14.5; rv:10.0) Gecko/20100101 Firefox/89.0"
+        }
+        if virtualhost:
+            headers["Host"] = virtualhost
+        res = requests.get(u, timeout=int(timeout), verify=False, headers=headers)
         res.raise_for_status()
         for k in res.headers.keys():
             if k not in blacklist:
-                if not new_headers.get(u, False):
-                    new_headers[u] = []
-                new_headers[u].append("{}: {}".format(k, res.headers[k]))
-            new_cookies[u] = dict(res.cookies)
+                data["headers"].append("{}: {}".format(k, res.headers[k]))
+            data["cookies"] = dict(res.cookies)
     except requests.exceptions.HTTPError as http_error:
         display_error("Http Error: {}".format(http_error))
     except requests.exceptions.ConnectionError as connect_error:
@@ -45,4 +55,4 @@ def process_urls(data):
         sys.exit(1)
     except Exception as e:
         display_error("{} no good, skipping: {}".format(u, e))
-    return (new_headers, new_cookies)
+    return data
